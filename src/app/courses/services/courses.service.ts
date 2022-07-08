f4bf6b918a06a48f1e57c1b53148b5c850495e50
Jhonatan Wesley Camêlo <jwcmade@gmail.com>
2022-07-07 22:55:43 -0300

    add new course's form error handling

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -10,14 +10,18 @@ export class CoursesService {
 
   private readonly API = 'api/courses';
 
-  constructor(private httpCliente: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   list() {
-    return this.httpCliente.get<Course[]>(this.API)
+    return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
         //delay(5000),
         tap(courses => console.log(courses))
       );
+  }
+
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
